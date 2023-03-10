0bfb24dbbc6aced2762670238b14a251f6b1fd2e
Ibrahim Doghri <patron.bara@gmail.com>
2023-03-10 10:44:21 +0100

    SEOs

--- README.md
+++ README.md
@@ -6,11 +6,16 @@ Planing to make it completely dynamic ad easy to use
 
 ## TODO
 
-- Fix layout overflow in (`/create`)
-- Add actual data in main (`/`)
-- Actually calculate the grades (maybe credits)
+- Fix layout overflow in (`/create`) ✅
+- Add actual data in main (`/`) ✅
+- Actually calculate the grades (maybe credits) ✅
+- Moyenne with 3 deciman points (19.125 instead of 19.1259123)
+- Better looking moyenne popup
 - Add feat to send the grades to the user through email or download pdf
-- Add a way for people to submit their templates to be saved
+- Add a way for people to submit their templates to be saved 
+- wa9telli yakhtar semester n3abi el université at the same time
+- Borders for Select in `/`
+
 
 ## Contribution & Feedback
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,22 @@
 <script>
     import "../app.css";
-  </script>
+</script>
   
+<svelte:head>
+  <!-- <meta name ="title" content ="Moycalc"/> -->
+  <meta name ="description" content ="A website for university students to calculate their average in case the administration didn't want to. Made by Arfizato"/>
+  <meta name ="copyright" content ="Arfizato"/>
+  <meta name ="keywords" content ="moycalc, arfizato, university"/>
+  <meta property ="og:type" content ="website"/>
+  <meta property ="og:url" content ="https://moycalc.arfizato.tn"/>
+  <meta property ="og:title" content ="Moycalc"/>
+  <meta property ="og:description" content ="A website for university students to calculate their average in case the administration didn't want to. Made by Arfizato"/>
+  <meta property ="og:image" content ="https://ik.imagekit.io/arfizato/moycalc.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1678441353507"/>
+  <meta property ="twitter:card" content ="summary_large_image"/>
+  <meta property ="twitter:url" content ="https://moycalc.arfizato.tn"/>
+  <meta property ="twitter:title" content ="Moycalc"/>
+  <meta property ="twitter:description" content ="A website for university students to calculate their average in case the administration didn't want to. Made by Arfizato"/>
+  <meta property ="twitter:image" content ="https://ik.imagekit.io/arfizato/moycalc.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1678441353507"/>
+
+</svelte:head>
   <slot />
